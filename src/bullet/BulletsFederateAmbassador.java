a3bbfe4bb3dae66d7e6d68954b7577c0c3af2e89
szadower <Adriank.kur@gmail.com>
2017-06-05 04:05:34 +0200

    Successful decode method - BulletsFederateAmbassador lines 126-141

--- src/bullet/BulletsFederateAmbassador.java
+++ src/bullet/BulletsFederateAmbassador.java
@@ -2,11 +2,14 @@ package bullet;
 
 import Helpers.Vector3;
 import hla.rti1516e.*;
+import hla.rti1516e.encoding.*;
+import hla.rti1516e.exceptions.FederateInternalError;
 import hla.rti1516e.time.HLAfloat64Time;
 
 public class BulletsFederateAmbassador extends NullFederateAmbassador {
 
     private BulletsFederate federate;
+
 
     protected double federateTime        = 0.0;
     protected double federateLookahead   = 1.0;
@@ -81,4 +84,98 @@ public class BulletsFederateAmbassador extends NullFederateAmbassador {
         this.isAdvancing = false;
     }
 
+
+    public void receiveInteraction( InteractionClassHandle interactionClass,
+                                    ParameterHandleValueMap theParameters,
+                                    byte[] tag,
+                                    OrderType sentOrdering,
+                                    TransportationTypeHandle theTransport,
+                                    SupplementalReceiveInfo receiveInfo )
+            throws FederateInternalError
+    {
+        // just pass it on to the other method for printing purposes
+        // passing null as the time will let the other method know it
+        // it from us, not from the RTI
+        this.receiveInteraction( interactionClass,
+                theParameters,
+                tag,
+                sentOrdering,
+                theTransport,
+                null,
+                sentOrdering,
+                receiveInfo );
+    }
+
+    @Override
+    public void receiveInteraction( InteractionClassHandle interactionClass,
+                                    ParameterHandleValueMap theParameters,
+                                    byte[] tag,
+                                    OrderType sentOrdering,
+                                    TransportationTypeHandle theTransport,
+                                    LogicalTime time,
+                                    OrderType receivedOrdering,
+                                    SupplementalReceiveInfo receiveInfo )
+            throws FederateInternalError
+    {
+        StringBuilder builder = new StringBuilder( "Interaction Received:" );
+
+        // print the handle
+        builder.append( " handle=" + interactionClass );
+        if( interactionClass.equals(federate.shotHandle) )
+        {
+            builder.append( " Czołg wystrzelił ! " );
+            HLAfixedArray<HLAfloat64BE> data =
+                    federate.encoderFactory.createHLAfixedArray( new DataElementFactory<HLAfloat64BE>()
+                                                                 {
+                                                                     public HLAfloat64BE createElement( int index )
+                                                                     {
+                                                                         return federate.encoderFactory.createHLAfloat64BE();
+                                                                     }
+                                                                 }, 3 );
+            builder.append(data.get(0).getValue()+"  aaaaaa ");
+            try {
+                data.decode(theParameters.get(federate.shotPositionHandle));
+            } catch (DecoderException e) {
+                e.printStackTrace();
+            }
+            builder.append(data.get(0).getValue());
+        }
+
+        // print the tag
+        builder.append( ", tag=" + new String(tag) );
+        // print the time (if we have it) we'll get null if we are just receiving
+        // a forwarded call from the other reflect callback above
+        if( time != null )
+        {
+            builder.append( ", time=" + ((HLAfloat64Time)time).getValue() );
+        }
+
+        // print the parameer information
+        builder.append( ", parameterCount=" + theParameters.size() );
+        builder.append( "\n" );
+        for( ParameterHandle parameter : theParameters.keySet() )
+        {
+            // print the parameter handle
+            builder.append( "\tparamHandle=" );
+            builder.append( parameter );
+            // print the parameter value
+            builder.append( ", paramValue=" );
+            builder.append( theParameters.get(parameter).length );
+            builder.append( " bytes" );
+            builder.append( "\n" );
+        }
+
+        log( builder.toString() );
+    }
+
+
+    private HLAfloat64BE[] wrapFloatData( float... data )
+    {
+        int length = data.length;
+        HLAfloat64BE[] array = new HLAfloat64BE[length];
+        for( int i = 0 ; i < length ; ++i )
+            array[i] = federate.encoderFactory.createHLAfloat64BE( data[i] );
+
+        return array;
+    }
 }
